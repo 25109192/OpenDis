2678f63c4eb66a74cca17c1ab9678c94c37c5bb3
Nicolas Bertin <bertin1@llnl.gov>
2026-05-29 16:53:30 -0700

    Added namespace to utilitity structs in types.h

--- src/types.h
+++ src/types.h
@@ -402,6 +402,8 @@ namespace Kokkos { //reduction identity must be defined in Kokkos namespace
  *-------------------------------------------------------------------------*/
 #include <Kokkos_Random.hpp>
 
+namespace ExaDiS {
+
 struct RandomGenerator {
     Kokkos::Random_XorShift64_Pool<Kokkos::Serial> random_pool_serial;
     Kokkos::Random_XorShift64_Pool<Kokkos::DefaultExecutionSpace> random_pool_device;
@@ -444,6 +446,8 @@ struct RandomGenerator {
     }
 };
 
+} // namespace ExaDiS
+
 /*---------------------------------------------------------------------------
  *
  *    Struct:       SortView
@@ -455,6 +459,8 @@ struct RandomGenerator {
 #include <thrust/device_ptr.h>
 #include <thrust/sort.h>
 #endif
+
+namespace ExaDiS {
 
 template <class ExecutionSpace = Kokkos::DefaultExecutionSpace>
 struct SortView
@@ -515,5 +521,7 @@ struct SortViewByKey<Kokkos::Cuda>
     }
 };
 #endif
+
+} // namespace ExaDiS
 
 #endif
